e97c03f20d5a6593debb5a4193c1acdf54251584
teddy931130 <info@teodordaskalov.com>
2019-08-30 12:55:25 +0300

    adding solution for problem #4

--- problem-#4-[Hard]-solution.py
+++ problem-#4-[Hard]-solution.py
@@ -1,4 +1,15 @@
+def missing_positive_int(n):
+    lowest = 1
+    n = [num for num in n if num > 0]
+
+    for num in sorted(n):
+        if num == lowest:
+            lowest += 1
+
+    print(lowest)
 
 
+numbers = list(map(int, input().split()))
+missing_positive_int(numbers)
 
 

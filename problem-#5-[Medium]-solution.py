1d2efc6692afc460b4cefab944577f6cba716532
teddy931130 <info@teodordaskalov.com>
2019-09-01 15:09:05 +0300

    adding solution for problem #5

--- problem-#5-[Medium]-solution.py
+++ problem-#5-[Medium]-solution.py
@@ -1,1 +1,16 @@
+def cons(a, b):
+    def pair(f):
+        return f(a, b)
+    return pair
 
+
+def car(func):
+    return func(lambda x, y: x)
+
+
+def cdr(func):
+    return func(lambda x, y: y)
+
+
+print(car(cons(3, 4)))
+print(cdr(cons(3, 4)))

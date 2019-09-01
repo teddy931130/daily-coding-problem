9472d15ddc5e4eb6d9b7d65d9dbed7fa2d7b58cb
Teodor Daskalov <info@teodordaskalov.com>
2019-09-01 09:15:54 +0300

    another small fix

--- problem-#3-[Medium]-solution.py
+++ problem-#3-[Medium]-solution.py
@@ -5,4 +5,30 @@ class Node:
         self.right = right
 
 
+def serialize(root):
+    serial = [root.val]
 
+    if root.left is None:
+        pass
+    else:
+        serial.append(serialize(root.left))
+
+    if root.right is None:
+        pass
+    else:
+        serial.append(serialize(root.right))
+    return serial
+
+
+def deserialize(s):
+    for index in range(len(s)):
+        if type(s[index]) is list:
+            s[index] = deserialize(s[index])
+
+    return Node(*s)
+
+
+if __name__ == "__main__":
+    node = Node('root', Node('left', Node('left.left')), Node('right'))
+
+    assert deserialize(serialize(node)).left.left.val == 'left.left'

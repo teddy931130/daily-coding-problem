e9e4f6b174a22005bc48e2ce3e4e6f2973fe10a9
teddy931130 <info@teodordaskalov.com>
2019-09-16 14:08:31 +0300

    update solution problem #11

--- problem-#11-[Medium]-solution.py
+++ problem-#11-[Medium]-solution.py
@@ -2,12 +2,12 @@ import re
 
 query_strings = ["foo", "foster", "folder", "fog",          # fo
                  "bar", "baby", "basket", "backend",        # ba
-                 "going", "gone", "goal", "gorgeous"        # go
+                 "going", "gone", "goal", "gorgeous",       # go
                  "self", "selfish", "selling", "sellout",   # se
                  "drop", "dropping", "drowning", "drone"]   # dr
 
 s = input()  # query string
 query_strings = '\n'.join(query_strings)  # transform list to one entry per line
-regex = r"^[" + re.escape(s) + r"]\w+\ *\w+"  # pattern: ^[s]\w+\ *\w+ where "s" is the variable
+regex = r"^[" + re.escape(s) + r"]\w+\ *\w+"  # pattern:  ^[s]\w+\ *\w+   <-- where "s" is the variable
 
 print(re.findall(regex, query_strings, re.MULTILINE))
